9c23e14d8fd80adcb4da09d8bbcadc619c163059
dnyimu <dnyimu98@gmail.com>
2025-09-04 20:10:13 -0400

    Add files via upload

---
+++ WINDOW FUNCTIONS- ROW NUMBER.sql
@@ -0,0 +1,67 @@
+-- RANKING MOST SOLD COLOUR IN 2024
+-- Whole World
+SELECT SUM(Sales_Volume), color, region,year,
+ROW_NUMBER()OVER( PARTITION BY region ORDER BY SUM(Sales_Volume) desc)  AS SalesRank
+FROM bmw_car_sales_classification
+where year = '2024'
+Group by color,region
+order by SUM( Sales_Volume) desc;
+
+
+-- Asia
+SELECT SUM( Sales_Volume), color, region, year,
+ROW_NUMBER() OVER( order by SUM( Sales_Volume) desc)  AS SalesRank
+FROM bmw_car_sales_classification
+where region= 'Asia' and year= '2024'
+Group by Color
+order by SUM( Sales_Volume) desc;
+
+-- North America
+SELECT SUM( Sales_Volume), color, region, year,
+ROW_NUMBER() OVER( order by SUM( Sales_Volume) desc)  AS SalesRank
+FROM bmw_car_sales_classification
+where region= 'North America' and year= '2024'
+Group by Color
+order by SUM( Sales_Volume) desc;
+
+-- South America
+SELECT SUM( Sales_Volume), color, region, year,
+ROW_NUMBER() OVER( order by SUM( Sales_Volume) desc)  AS SalesRank
+FROM bmw_car_sales_classification
+where region= 'South America' and year= '2024'
+Group by Color
+order by SUM( Sales_Volume) desc;
+
+-- Europe
+SELECT SUM( Sales_Volume), color, region, year,
+ROW_NUMBER() OVER( order by SUM( Sales_Volume) desc)  AS SalesRank
+FROM bmw_car_sales_classification
+where region= 'Europe' and year= '2024'
+Group by Color
+order by SUM( Sales_Volume) desc;
+
+
+-- Middle East
+SELECT SUM( Sales_Volume), color, region, year,
+ROW_NUMBER() OVER( order by SUM( Sales_Volume) desc)  AS SalesRank
+FROM bmw_car_sales_classification
+where region= 'Middle East' and year= '2024'
+Group by Color
+order by SUM( Sales_Volume) desc;
+
+-- Africa
+SELECT SUM( Sales_Volume), color, region, year,
+ROW_NUMBER() OVER( order by SUM( Sales_Volume) desc)  AS SalesRank
+FROM bmw_car_sales_classification
+where region= 'AFRICA' and year= '2024'
+Group by Color
+order by SUM( Sales_Volume) desc;
+
+
+
+
+
+
+
+
+
