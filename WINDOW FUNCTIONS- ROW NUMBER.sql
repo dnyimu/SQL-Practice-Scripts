1a861ff85bc143536f9dc63a7adeb1edfdc83504
dnyimu <dnyimu98@gmail.com>
2025-09-13 19:07:08 -0400

    Update WINDOW FUNCTIONS- ROW NUMBER.sql

--- WINDOW FUNCTIONS- ROW NUMBER.sql
+++ WINDOW FUNCTIONS- ROW NUMBER.sql
@@ -1,10 +1,10 @@
 -- RANKING MOST SOLD COLOUR IN 2024
 -- Whole World
-SELECT SUM(Sales_Volume), color, region,year,
-ROW_NUMBER()OVER( PARTITION BY region ORDER BY SUM(Sales_Volume) desc)  AS SalesRank
+SELECT SUM(Sales_Volume), Color,year, 
+ROW_NUMBER() OVER( order by SUM( Sales_Volume) desc)  AS SalesRank
 FROM bmw_car_sales_classification
 where year = '2024'
-Group by color,region
+Group by Color
 order by SUM( Sales_Volume) desc;
 
 
@@ -55,6 +55,64 @@ ROW_NUMBER() OVER( order by SUM( Sales_Volume) desc)  AS SalesRank
 FROM bmw_car_sales_classification
 where region= 'AFRICA' and year= '2024'
 Group by Color
+order by SUM( Sales_Volume) desc;
+--------------------------------------------------------------------
+-- RANKING MOST SOLD Models in 2024
+
+-- Whole World
+SELECT SUM(Sales_Volume), Model ,year, 
+ROW_NUMBER() OVER( order by SUM( Sales_Volume) desc)  AS SalesRank
+FROM bmw_car_sales_classification
+where year = '2024'
+Group by Model
+order by SUM( Sales_Volume) desc;
+
+-- Asia
+SELECT SUM( Sales_Volume), model, region, year,
+ROW_NUMBER() OVER( order by SUM( Sales_Volume) desc)  AS SalesRank
+FROM bmw_car_sales_classification
+where region= 'Asia' and year= '2024'
+Group by model
+order by SUM( Sales_Volume) desc;
+
+-- North America
+SELECT SUM( Sales_Volume), Model, region, year,
+ROW_NUMBER() OVER( order by SUM( Sales_Volume) desc)  AS SalesRank
+FROM bmw_car_sales_classification
+where region= 'North America' and year= '2024'
+Group by Model
+order by SUM( Sales_Volume) desc;
+
+-- South America
+SELECT SUM( Sales_Volume), model, region, year,
+ROW_NUMBER() OVER( order by SUM( Sales_Volume) desc)  AS SalesRank
+FROM bmw_car_sales_classification
+where region= 'South America' and year= '2024'
+Group by Model
+order by SUM( Sales_Volume) desc;
+
+-- Europe
+SELECT SUM( Sales_Volume), model, region, year,
+ROW_NUMBER() OVER( order by SUM( Sales_Volume) desc)  AS SalesRank
+FROM bmw_car_sales_classification
+where region= 'Europe' and year= '2024'
+Group by Model
+order by SUM( Sales_Volume) desc;
+
+-- Middle East
+SELECT SUM( Sales_Volume), color, region, year,
+ROW_NUMBER() OVER( order by SUM( Sales_Volume) desc)  AS SalesRank
+FROM bmw_car_sales_classification
+where region= 'Middle East' and year= '2024'
+Group by Color
+order by SUM( Sales_Volume) desc;
+
+-- Africa
+SELECT SUM( Sales_Volume), Model, region, year,
+ROW_NUMBER() OVER( order by SUM( Sales_Volume) desc)  AS SalesRank
+FROM bmw_car_sales_classification
+where region= 'AFRICA' and year= '2024'
+Group by Model
 order by SUM( Sales_Volume) desc;
 
 
